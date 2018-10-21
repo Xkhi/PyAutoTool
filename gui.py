9e0c379318528983f9182e58d34f1439304be771
Javier Alvarez <jabetec@hotmail.com>
2018-10-20 21:44:24 -0500

    Add compatibility with python 3 Menu Bar added with limited functionality, which includes exit and about

--- gui.py
+++ gui.py
@@ -1,13 +1,19 @@
+from __future__ import print_function
 import appJar
 import pyautogui as pag
+
 
 
 class App(object):    
     def __init__(self):
         #General configuration
         self.app = appJar.gui("Python Automation Tool","480x360")
-        self.app.addButton("Save Position",self.getBtn,1,1)
-        self.app.addButton("Save Typing",self.getBtn,2,1)
+        #Menu Bar
+        self.app.addMenuList("File", ["Save","-","Exit"], self.genericButton)
+        self.app.addMenuList("Help",["Help Contents","-","About PyAuTo"],self.genericButton)
+        #Buttons
+        self.app.addButton("Save Position",self.genericButton,1,1)
+        self.app.addButton("Save Typing",self.genericButton,2,1)
         self.app.addLabel("TypeArea","Type Area",0,2)
         self.app.addTextArea("typedef",1,2)
         #Status bar configuration
@@ -19,11 +25,19 @@ class App(object):
         self.app.registerEvent(self.position_cbk)
         self.app.setPollTime(100)
 
-    def getBtn(self,btn):
-        print "Button pressed",btn
+    def genericButton(self,btn):
+        if btn == 'Exit':
+            self.stop()
+        elif btn == 'About PyAuTo':
+            self.app.infoBox("About PyAuTo", "This tool was created by\nJavier Alvarez & Enrique Espinosa\nFor Continental Engineering Services")
+        else:
+            print ("Button pressed",btn, "Not yet implemented")
 
     def start(self):
         self.app.go()
+    
+    def stop(self):
+        self.app.stop()
 
     def position_cbk(self):
         x,y = pag.position()
