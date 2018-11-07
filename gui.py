9e5789bcb99b29145e6c3d83ce317f887da3112f
Javier Alvarez <javier.3.alvarez@continental-corporation.com>
2018-11-07 16:32:39 -0500

    Organize some code Add functionality for most buttons:     Text parsing complete     Click parsing complete     Save file complete Add .gitignore to avoid pesky files from eclipse Create utils.py with constants and useful commands to build file
    Program can now save text, keys, hotkeys and clicks into an executable python script

--- gui.py
+++ gui.py
@@ -1,114 +1,125 @@
 from __future__ import print_function
-import appJar
 import pyautogui as pag
+import appJar
+import utils
 import os
+from nt import lstat
 
-
-
-
-class App(object):    
+class App(object):
     def __init__(self):
+        #Local properties
+        self.xpos = 0
+        self.ypos = 0
+
+        self.storedXpos = 0
+        self.storedYpos = 0
+
+        self.commandList = []
+        self.pythonCommands = []
         #General configuration
         self.app = appJar.gui("Python Automation Tool","1280x720")
-        
         #Menu Bar
         self.app.addMenuList("File", ["Save","-","Exit"], self.genericButton)
         self.app.addMenuList("Help",["Help Contents","-","About PyAuTo"],self.genericButton)
-
-        
         ##Row1
         guiRow = 0
         self.app.addLabel("Click","Click",guiRow,0)
         self.app.addLabel("Button","Button",guiRow,1)
         self.app.addLabel("Text","Text",guiRow,2)
-        
         ##Row2
         guiRow = 1
-        self.app.addButton("Get Click Position",self.getClickPositionButton,guiRow,0)
-        self.app.addOptionBox("Type Kind", ["Plain Text", "Key Combo"], guiRow, 2)
-        
+        self.app.addButton("Capture Click Position",self.getClickPositionButton,guiRow,0)
+        self.app.addOptionBox("Type Kind", ["Plain Text", "Key Combo","Single Press"], guiRow, 2)
         ##Row3
         guiRow = 2
         self.app.addLabel("Click Position","",guiRow,0)
         self.app.addLabelFileEntry("Select image", guiRow, 1)
         self.app.addTextArea("TypeArea",guiRow,2)
-        
         ##Row4
         guiRow = 3
-        self.app.addButton("Save Click Position",self.genericButton,guiRow,0)
+        self.app.addButton("Save Click Position",self.getClickPositionButton,guiRow,0)
         self.app.addButton("Save Button",self.genericButton,guiRow,1)
-        self.app.addButton("Save Typing",self.genericButton,guiRow,2)
-        
+        self.app.addButton("Save Typing",self.parseTextCommands,guiRow,2)
         ##Row5
         guiRow = 4
-        #self.app.addLabel("CommandListLabel","Command List",guiRow,0)
         self.app.addLabelEntry("Delay(s)",guiRow , 0)
         self.app.addButton("Clear Last Command",self.clearLastCommandButton,guiRow,2)
-        #self.app.addButton("Clear All Commands",self.clearAllCommandsButton,guiRow,2)
-        #self.app.addLabel("commlist","Command List",Label2Level,0)
-        
         ##Row6
         guiRow = 5
         self.app.addScrolledTextArea("CommandList",guiRow,0,3)
-        
         ##Row7
         guiRow = 6
-        self.app.addLabelEntry("AutoScrip File Name",guiRow , 0)
+        self.app.addLabelEntry("AutoScript File Name",guiRow , 0)
         self.app.addButton("Save AutoScript File",self.genericButton,guiRow,2)
-        
-        
         #Status bar configuration
         self.app.addStatusbar(fields=3)
         self.app.setStatusbar("Mouse",0)
-        self.app.setStatusbar("x: ",1)
-        self.app.setStatusbar("y: ",2)
+        self.app.setStatusbar("X: ",1)
+        self.app.setStatusbar("Y: ",2)
         #Status bar event polling
-        self.app.registerEvent(self.position_cbk)
+        self.app.registerEvent(self.positionCbk)
         self.app.setPollTime(100)
         self.app.setEntry("Delay(s)", "0.01")
-        
-        self.xpos = 0
-        self.ypos = 0
-        
-        self.storedXpos = 0
-        self.storedYpos = 0
-        
-        self.commandList = []
 
     def genericButton(self,btn):
         if btn == 'Exit':
             self.stop()
         elif btn == 'About PyAuTo':
-            self.app.infoBox("About PyAuTo", "This tool was created by\nJavier Alvarez & Enrique Espinosa\nFor Continental Engineering Services")
+            self.app.infoBox("About PyAuTo", utils.about)
+        elif btn == 'Save AutoScript File':
+            with open(self.app.getEntry("AutoScript File Name"),'w') as outfile:
+                outfile.write(utils.buildFile(self.pythonCommands))
         else:
-            self.commandList.append("Button pressed"+btn+"Not yet implemented")
-            print ("Button pressed",btn, "Not yet implemented")
-            
+            self.commandList.append("Button pressed {}, not yet implemented".format(btn))
+
             self.app.setEntry("Delay(s)", "0.01")
-            self.app.clearTextArea("CommandList")
-            self.app.setTextArea("CommandList",self.writeCommands())
-            
+            self.writeCommands()
+
+    def parseTextCommands(self,btn):
+        opt = self.app.getOptionBox("Type Kind")
+        txt = self.app.getTextArea("TypeArea").lower()
+        pause = self.app.getEntry("Delay(s)")
+        #Plain text handler
+        if(opt == "Plain Text"):
+            self.commandList.append("Plain write '{}'".format(txt))
+            self.pythonCommands.append(utils.write(txt,pause))
+        #Key Combo handler
+        elif (opt == "Key Combo"):
+            lst = [item for item in txt.split() if item in utils.keys]
+            self.commandList.append("Key Combo '{}'".format("+".join(lst)))
+            self.pythonCommands.append(utils.hotkey(lst,pause))
+        #Single Press handler
+        elif (opt == "Single Press" and txt in utils.keys):
+            self.commandList.append("Single Press '{}'".format(txt))
+            self.pythonCommands.append(utils.press(txt,pause))
+        #Invalid Handler
+        else:
+            print("Invalid Option")
+        #Post-parse actions
+        self.app.clearTextArea("TypeArea")
+        self.writeCommands()
+
     def getClickPositionButton(self,btn):
-        
+        duration = self.app.getEntry("Delay(s)")
+        if (btn == "Capture Click Position"):
             self.storedXpos = self.xpos
             self.storedYpos = self.ypos
-            
-            self.app.setLabel("Click Position","x:"+str(self.storedXpos)+",y:"+str(self.storedYpos))
-            print ("Position Stored",self.storedXpos,self.storedYpos)
-            print(self.app.getEntry("Delay(s)"))
-            
+            self.app.setLabel("Click Position","X:"+str(self.storedXpos)+",Y:"+str(self.storedYpos))
+        elif (btn == "Save Click Position"):
+            x,y = self.app.getLabel("Click Position").split(',')
+            self.commandList.append("Click on position X:{} Y:{}".format(x[2:],y[2:]))
+            self.pythonCommands.append(utils.click(x[2:],y[2:],duration))
+            self.writeCommands()
+
     def clearLastCommandButton(self,btn):
-        
         if len(self.commandList) == 0:
             self.app.clearTextArea("CommandList")
             self.app.setTextArea("CommandList","Command List Empty!")
         else:
             self.commandList.pop()
-            self.app.clearTextArea("CommandList")
-            self.app.setTextArea("CommandList",self.writeCommands())
-            
+            self.writeCommands()
+
     def clearAllCommandsButton(self,btn):
-        
         if len(self.commandList) == 0:
             self.app.clearTextArea("CommandList")
             self.app.setTextArea("CommandList","Command List Empty!")
@@ -116,31 +127,28 @@ class App(object):
             self.commandList = []
             self.app.clearTextArea("CommandList")
             self.app.setTextArea("CommandList","Command List Empty!")
-            
+
     def start(self):
         self.app.go()
-    
+
     def stop(self):
         self.app.stop()
 
-    def position_cbk(self):
-        x,y = pag.position()
-        self.app.setStatusbar("x: "+str(x),1)
-        self.app.setStatusbar("y: "+str(y),2)
-        
-        self.xpos = x
-        self.ypos = y
-        
+    def positionCbk(self):
+        self.xpos,self.ypos = pag.position()
+        self.app.setStatusbar("x: "+str(self.xpos),1)
+        self.app.setStatusbar("y: "+str(self.ypos),2)
+
     def writeCommands(self):
-        
+        self.app.clearTextArea("CommandList")
         commandText = ""
-        
+
         for command in self.commandList:
             commandText = commandText + command + "\n"
-        
-        return commandText
-        
-        
+
+        self.app.setTextArea("CommandList", commandText)
+        print(self.pythonCommands)
+
 
 def main():
     new = App()
---
+++ utils.py
@@ -0,0 +1,44 @@
+keys = ['\t', '\n', '\r', ' ', '!', '"', '#', '$', '%', '&', "'", '(',
+')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7',
+'8', '9', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`',
+'a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~',
+'accept', 'add', 'alt', 'altleft', 'altright', 'apps', 'backspace',
+'browserback', 'browserfavorites', 'browserforward', 'browserhome',
+'browserrefresh', 'browsersearch', 'browserstop', 'capslock', 'clear',
+'convert', 'ctrl', 'ctrlleft', 'ctrlright', 'decimal', 'del', 'delete',
+'divide', 'down', 'end', 'enter', 'esc', 'escape', 'execute', 'f1', 'f10',
+'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17', 'f18', 'f19', 'f2', 'f20',
+'f21', 'f22', 'f23', 'f24', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+'final', 'fn', 'hanguel', 'hangul', 'hanja', 'help', 'home', 'insert', 'junja',
+'kana', 'kanji', 'launchapp1', 'launchapp2', 'launchmail',
+'launchmediaselect', 'left', 'modechange', 'multiply', 'nexttrack',
+'nonconvert', 'num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6',
+'num7', 'num8', 'num9', 'numlock', 'pagedown', 'pageup', 'pause', 'pgdn',
+'pgup', 'playpause', 'prevtrack', 'print', 'printscreen', 'prntscrn',
+'prtsc', 'prtscr', 'return', 'right', 'scrolllock', 'select', 'separator',
+'shift', 'shiftleft', 'shiftright', 'sleep', 'space', 'stop', 'subtract', 'tab',
+'up', 'volumedown', 'volumemute', 'volumeup', 'win', 'winleft', 'winright', 'yen',
+'command', 'option', 'optionleft', 'optionright']
+
+about = "This tool was created by\nJavier Alvarez & Enrique Espinosa\nFor Continental Engineering Services"
+
+fileHeader = "import pyautogui as pag\nfrom time import sleep\n\ndef main():\n"
+fileFooter = "\nif __name__ == '__main__':\n    main()"
+
+def press(btn,pause=0.1):
+    return "    pag.press('{}',pause={})\n".format(btn,pause)
+
+def write(str,pause=0.1):
+    return "    pag.typewrite('{}',pause={})\n".format(str,pause)
+
+#FAULTY FUCNTION, NEEDS CHECKING
+def hotkey(keys,pause=0.1):
+    return "    pag.hotkey({},pause={})\n".format(",".join(keys),pause)
+
+def click(x,y,duration=0.1):
+    return "    pag.click({},{},duration={})\n".format(x,y,duration)
+
+def buildFile(commandList):
+    commands = "".join(commandList)
+    return fileHeader+commands+fileFooter
